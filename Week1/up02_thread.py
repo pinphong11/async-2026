0f9c0f990e0788e8609bf41f0bc4110f38b46ded
pinphong <pinphongchusuwan@gmail.com>
2026-06-24 11:35:50 +0700

    up2

--- Week1/up02_thread.py
+++ Week1/up02_thread.py
@@ -32,9 +32,6 @@ def main():
             name=f"Thread-{customer}"
         )
         threads.append(t)
-
-    # สั่งให้ทุก Thread เริ่มทำงาน
-    for t in threads:
         t.start()
 
     # รอให้ทุก Thread ทำงานเสร็จ
